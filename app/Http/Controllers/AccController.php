76b3859bab55995d7808bc0911ffd6cb6552ab23
Johsua Burce <burcejosh19@gmail.com>
2026-01-06 14:28:35 +0800

    save

---
+++ app/Http/Controllers/AccController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class AccController extends Controller
+{
+    //
+    public account(){
+        return Inertia::render('')
+    }
+}
